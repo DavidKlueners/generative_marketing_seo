db71a6f0a58ce0a68b2c4c9125f17f29bcf355aa
David Klüners <davidklueners@gmail.com>
2024-01-19 12:38:37 +0100

    added some initial messages

--- app.py
+++ app.py
@@ -3,11 +3,31 @@ from runnables import prompt_input_chain
 
 
 @cl.on_chat_start
-def start_chat():
+async def main():
     cl.user_session.set(
         "message_history",
         [{"role": "system", "content": "You are a helpful assistant."}],
     )
+    res = await cl.AskUserMessage(
+        content="Please provide a comma separated list of website URLs here, the first one should be your own website, the other ones are relevant competitor websites.",
+        timeout=10,
+    ).send()
+    if res:
+        await cl.Message(
+            content=f"The website links you provided are: {res['output']}.",
+        ).send()
+        res = await cl.AskActionMessage(
+            content="If these links are correct, press 'Continue'!",
+            actions=[
+                cl.Action(name="continue", value="continue", label="✅ Continue"),
+                cl.Action(name="cancel", value="cancel", label="❌ Cancel"),
+            ],
+        ).send()
+
+        if res and res.get("value") == "continue":
+            await cl.Message(
+                content="Continue!",
+            ).send()
 
 
 @cl.on_message
@@ -18,7 +38,9 @@ async def main(message: cl.Message):
     msg = cl.Message(content="")
     await msg.send()
 
-    async for token in prompt_input_chain.astream(input={"user_question": msg.content, "conversation_history" : message_history}):
+    async for token in prompt_input_chain.astream(
+        input={"user_question": msg.content, "conversation_history": message_history}
+    ):
         await msg.stream_token(token)
 
     message_history.append({"role": "assistant", "content": msg.content})
